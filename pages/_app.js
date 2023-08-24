869da7bf6ff8081bd3eadae814c37330b37b4382
Dev-KhemWitchayut <s.wichayut@softkingdoms.com>
2023-08-24 13:15:18 +0700

    imp antd

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import { store } from "../store/store";
 import { getToken } from "../store/slices/userSlice";
 import { createTheme, ThemeProvider } from '@mui/material';
+import { Table, Input, Space, } from 'antd';
 
 function MyApp({ Component, pageProps }) {
 
